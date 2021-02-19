7ae7d11d6b118b43aed6c8d32a3921708f775774
work2544 <work21_123@hotmail.com>
2021-02-19 17:31:04 +0700

    complete

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -3,7 +3,6 @@
 #include<vector>
 #include<string>
 #include<cstdlib>
-
 using namespace std;
 
 char score2grade(int score){
@@ -20,20 +19,69 @@ string toUpperStr(string x){
     return y;
 }
 
-void importDataFromFile(){
-
+void importDataFromFile(string filename,vector<string> &names,vector<int> &score,vector<char> &grades){
+ifstream table(filename.c_str());
+string textline;
+char format[]="%[^:]: %d %d %d";
+int j=0;
+while(getline(table,textline))
+{
+    int totalscore[3];
+    char nametemp[100];
+    sscanf(textline.c_str(),format,nametemp,&totalscore[0],&totalscore[1],&totalscore[2]);
+    names.push_back(nametemp);
+    for (int i = 1; i < 3; i++)
+    {
+        totalscore[0]+=totalscore[i];
+    }
+    score.push_back(totalscore[0]);
+    grades.push_back(score2grade(score[j]));
+    j++;
+}
+table.close();
 }
 
-void getCommand(){
-
+void getCommand(string &command,string &keys){
+string text;
+cout<<"Please input your command: ";
+getline(cin,text);
+char format[]="%s %[^:]";
+char com[100],k[100];
+sscanf(text.c_str(),format,com,k);
+command=com;
+keys=k;
 }
 
-void searchName(){
-
+void searchName(vector<string> &names,vector<int> &score,vector<char> &grade,string key){
+cout<<"---------------------------------\n";
+int k=0;
+for (unsigned int i = 0; i < names.size(); i++)
+{
+   
+    string temp=toUpperStr(names[i]);
+    if(temp==key)
+    {
+        cout<<names[i]<<"'s score = "<<score[i]<<endl;
+        cout<<names[i]<<"'s grade = "<<grade[i]<<endl;
+        k++;
+    }
+}
+if(k==0)cout<<"Cannot found.\n";
+cout<<"---------------------------------\n";
 }
 
-void searchGrade(){
+void searchGrade(vector<string> &names,vector<int> &score,vector<char> &grade,string key){
 
+cout<<"---------------------------------\n";
+for (unsigned int i = 0; i < names.size(); i++)
+{
+    char kk=key[0];
+    if(grade[i]==kk)
+    {
+        cout<<names[i]<<" ("<<score[i]<<")"<<endl;
+    }
+}
+cout<<"---------------------------------\n";
 }
 
 
